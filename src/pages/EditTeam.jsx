7752c96dc0b825cb7664c9e8add403b3f9ac8652
Arjun Malhotra <arjunhero38@gmail.com>
2025-02-14 13:46:49 +0530

    Fixed uuid import issue

--- src/pages/EditTeam.jsx
+++ src/pages/EditTeam.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { client } from "../sanityClient";
 import { useNavigate, useParams } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid"; // For generating unique keys
+import { v4 as uuidv4 } from "uuid";
 
 const EditTeam = () => {
   const { id } = useParams();
